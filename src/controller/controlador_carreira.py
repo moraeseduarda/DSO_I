7e1f4a56efa74f5a860aac05f86cb5c35c831d98
moraeseduarda <eduardamorads@gmail.com>
2025-05-05 19:56:02 -0300

    feat: implementa controlador carreira e tela carreira

---
+++ src/controller/controlador_carreira.py
@@ -0,0 +1,51 @@
+from model.carreira import Carreira
+from view.tela_carreira import TelaCarreira
+
+class ControladorCarreira():
+    def __init__(self):
+        self.__controlador_sistema = controlador_sistema
+        self.__tela_carreira = TelaCarreira()
+        self.__carreiras = []
+
+    def pega_carreira_por_id(self, id: int):
+        for carreira in self.__carreiras:
+            if carreira.id == id:
+                return carreira
+        return None
+    
+    def incluir_carreira(self):
+        dados_carreira = self.__tela_carreira.pega_dados_carreira()
+        nova_carreira = Carreira(dados_carreira['id'], dados_carreira['nome'], dados_carreira['descricao'])
+        self.__carreiras.append(nova_carreira)
+        self.__tela_carreira.mostra_mensagem('Carreira cadastrada com sucesso!')
+    
+    def alterar_carreira(self):
+        self.__carreiras
+        
+    def lista_carreira(self):
+        for carreira in self.__carreiras:
+            self.__tela_carreira.mostra_carreira({'id': carreira.id, 'nome': carreira.nome, 'descricao': carreira.descricao})
+        if self.__carreiras == []:
+            self.__tela_carreira.mostra_mensagem('Nenhuma carreira cadastrada!')
+        
+    def excluir_carreira(self):
+        self.__lista_carreira()
+        id_carreira = self.__tela_carreira.seleciona_carreira()
+        carreira = self.pega_carreira_por_id(id_carreira)
+        
+        if carreira is not None:
+            self.__carreiras.remove(carreira)
+            self.lista_carreira()
+        else:
+            self.__tela_carreira.mostra_mensagem('Carreira não exite.')
+            
+    def retornar(self):
+        self.__controlador_sistema.abre_tela()
+        
+    def abre_tela(self):
+        lista_opcoes = {1: self.incluir_carreira, 2: self.alterar_carreira, 3: self.lista_carreira, 4: self.excluir_carreira, 0: self.retornar}
+        
+        continua = True
+        while continua:
+            lista_opcoes[self.__tela_livro.tela_opcoes()]()
+            
---
+++ src/view/tela_carreira.py
@@ -0,0 +1,32 @@
+class TelaCarreira:    
+    def tela_opcoes(self):
+        print('SISTEMA DE MONITORAMENTO DE HARD SKILLS')
+        print('Escolha uma opção:')
+        print('1 - Incluir carreira')
+        print('2 - Alterar carreira')
+        print('3 - Excluir carreira')
+        print('4 - Listar carreira')
+        print('Retornar')
+        opcao = int(input('Digite a opção desejada: '))
+        return opcao
+    
+    def pega_dados_carreira(self):
+        print('---------- CARREIRA ----------')
+        id = int(input('Digite o ID da carreira: '))
+        nome = input('Digite o nome da carreira: ')
+        descricao = input('Digite a descrição da carreira: ')
+        
+        return {'id': id, 'nome': nome, 'descricao': descricao}
+    
+    def mostra_carreira(self, dados_carreira):
+        print('ID DO CARREIRA: ' + str(dados_carreira['carreira'].id))
+        print('TITULO DO CARREIRA: ' + dados_carreira['carreira'].titulo)
+        print('DESCRICAO DO CARREIRA: ' + dados_carreira['carreira'].descricao)
+        print('\n')
+        
+    def seleciona_carreira(self):
+        id = int(input('Digite o ID da carreira: '))
+        return id
+    
+    def mostra_mensagem(self, mensagem):
+        print(mensagem)
