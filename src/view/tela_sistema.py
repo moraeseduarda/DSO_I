fa1c3d90a2a5746ea75ee5a9fb7e624fd289b822
cldhelena <helena.callado@gradufsc.com.br>
2025-05-05 19:24:48 -0300

    feat: implementa a classe TelaSistema com opções de menu

---
+++ src/view/tela_sistema.py
@@ -0,0 +1,10 @@
+class TelaSistema:
+    def tela_opcoes(self):
+        print('SISTEMA DE MONITORAMENTO DE HARD SKILLS')
+        print('Escolha uma opção:')
+        print('1 - CARREIRA')
+        print('2 - SKILL')
+        print('3 - USUÁRIO')
+        print('0 - SAIR')
+        opcao = int(input('Digite a opção desejada: '))
+        return opcao
