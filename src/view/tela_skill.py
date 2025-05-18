026256c8250aa3b6617b4f99b680288954a89cd6
cldhelena <helena.callado@gradufsc.com.br>
2025-05-18 17:45:51 -0300

    feat: adiciona métodos para manipulação de habilidades na tela de skills

--- src/view/tela_skill.py
+++ src/view/tela_skill.py
@@ -7,4 +7,23 @@ class TelaSkill:
         print('4 - Listar habilidade')
         print('5 - Retornar')
     
+    def pega_dados_skill(self):
+        print('-----HABILIDADE-----')
+        id = int(input('Digite o ID da habilidade: '))
+        nome = input('Digite o nome da habilidade: ')
+        descricao = input('Digite a descrição da habilidade: ')
+        return {'id':id,'nome':nome, 'descricao':descricao}
+    
+    def mostra_skill(self, dados_skill):
+        print('ID DA HABILIDADE: ', dados_skill['id'])
+        print('NOME DA HABILIDADE: ', dados_skill['nome'])
+        print('DESCRICAO DA HABILIDADE: ', dados_skill['desricao'])
+    
+    def seleciona_skill(self):
+        id = int(input('Digite o ID da habiliadde: '))
+        return id
+    
+    def mostra_mensagem(self, mensagem):
+        print(mensagem)
+    
     
