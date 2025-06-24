fe73936e6f162b5431141f5455f3b93c4d9dff90
cldhelena <helena.callado@gradufsc.com.br>
2025-06-23 23:51:21 -0300

    feat: replace console input with GUI for initial options in TelaSistema

--- src/view/tela_sistema.py
+++ src/view/tela_sistema.py
@@ -1,15 +1,30 @@
+import PySimpleGUI as sg
 from view.console_utils import limpar_console
 
 
 class TelaSistema:
     def tela_opcoes_iniciais(self):
-        print("===== BEM-VINDO AO SISTEMA DE MONITORAMENTO DE HARD SKILLS =====")
-        print("1 - Entrar como Administrador")
-        print("2 - Entrar como Usuário")
-        print("0 - Sair")
-        opcao = int(input("Digite a opção desejada: "))
+        layout = [
+            [sg.Text("===== BEM-VINDO AO SISTEMA DE MONITORAMENTO DE HARD SKILLS =====")],
+            [sg.Button("Entrar como Administrador", key="1")],
+            [sg.Button("Entrar como Usuário", key="2")],
+            [sg.Button("Sair", key="0")]
+        ]
+        window = sg.Window("Sistema de Monitoramento", layout)
+        while True:
+            event, _ = window.read()
+            if event in ("0", sg.WIN_CLOSED):
+                opcao = 0
+                break
+            elif event == "1":
+                opcao = 1
+                break
+            elif event == "2":
+                opcao = 2
+                break
+        window.close()
         limpar_console()
         return opcao
-    
+
     def mostra_mensagem(self, mensagem):
-        print(mensagem)
+        sg.popup(mensagem)
