c30b10d8754982da7e3cb911c214b3023bf71ba7
moraeseduarda <90060846+moraeseduarda@users.noreply.github.com>
2025-06-28 19:00:42 -0300

    fix: corrige tipos de dados para username e chave nas funções do UsuarioDAO

--- src/DAOs/usuarios_dao.py
+++ src/DAOs/usuarios_dao.py
@@ -7,17 +7,17 @@ class UsuarioDAO(DAO):
         super().__init__('usuario.pkl')
 
     def add(self, usuario: Usuario):
-        if((usuario is not None) and isinstance(usuario, Usuario) and isinstance(usuario.username, int)):
+        if((usuario is not None) and isinstance(usuario, Usuario) and isinstance(usuario.username, str)):
             super().add(usuario.username, usuario)
 
     def update(self, usuario: Usuario):
-        if((usuario is not None) and isinstance(usuario, Usuario) and isinstance(usuario.username, int)):
+        if((usuario is not None) and isinstance(usuario, Usuario) and isinstance(usuario.username, str)):
             super().update(usuario.username, usuario)
 
-    def get(self, key:int):
-        if isinstance(key, int):
+    def get(self, key:str):
+        if isinstance(key, str):
             return super().get(key)
 
-    def remove(self, key:int):
-        if(isinstance(key, int)):
+    def remove(self, key:str):
+        if(isinstance(key, str)):
             return super().remove(key)
